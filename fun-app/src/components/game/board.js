c5175ceaaf01608ea503cbe9ca23ce3c2c6d83d6
Amanpreet Singh <coool.aman@gmail.com>
2019-03-20 16:14:53 +0530

    Used Loops

--- fun-app/src/components/game/board.js
+++ fun-app/src/components/game/board.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import './board.css'
 
 class Square extends React.Component {
     // TODO: remove the constructor
@@ -32,20 +33,28 @@ export default class Board extends React.Component {
         return <Square value={this.state.squares[i]}/>;
     }
 
+    renderBoardRow(n) {
+        let cells = [];
+        for (let i = n; i < n + 3; i++) {
+            cells.push(this.renderSquare(i));
+        }
+        return (<div className="board-row">
+            {cells}
+        </div>);
+    }
+
     render() {
         const status = 'Next player: X';
+        let rows = []
+        for (let i = 0; i < 3; i++) {
+            rows.push(this.renderBoardRow(i * 3))
+        }
 
         return (
             <div>
                 <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+                <div>
+                    {rows}
                 </div>
             </div>
         );
